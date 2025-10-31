b187485185d2cb09cefceb5007451fe680294039
camille <caauvity@orange.fr>
2025-10-31 16:55:16 -0300

    test new big

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".recent-projects-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      900: { slidesPerView: 2 },
+      1200: { slidesPerView: 3 },
+    },
+  });
+});
